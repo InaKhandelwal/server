09eef65965ddff78a2b677c20fe8165eb98f3099
Ina <khandelwal.inaa@gmail.com>
2017-10-09 11:33:49 +0530

    code for server

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 #SERVER CODE
+# import socket library
 import socket
-
+#create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#reserve a port in your computer
 serversocket.bind(('localhost', 8000))
+# put socket into listening mode
 serversocket.listen(5) # become a server socket, maximum 5 connections
 
 while True:
