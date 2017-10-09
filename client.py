0ba9946a5e2857909265d2089804d133dcd9ae6c
Ina <khandelwal.inaa@gmail.com>
2017-10-09 11:35:06 +0530

    update client.py

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 #CLIENT CODE
+#import socket library
 import socket
+#import subprocess library
 import subprocess
-    
+#create an INET, STREAMing socket
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#reserve a port in your computer
 clientsocket.connect(('localhost', 8000))
 while True:
     test = subprocess.Popen(["netstat","-np","TCP"],stdout=subprocess.PIPE)
